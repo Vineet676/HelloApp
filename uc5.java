d6b1403f6171f01324ec2ad4edc0f7e0a93575d9
Vineet676 <vineetnair1534@gmail.com>
2026-04-01 21:28:18 +0530

    Implement UC5 - Repeat Greeting Multiple Times

---
+++ uc5.java
@@ -0,0 +1,17 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        // If no arguments are passed
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } 
+        else {
+            // Enhanced for loop
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
+        }
+
+    }
+}
